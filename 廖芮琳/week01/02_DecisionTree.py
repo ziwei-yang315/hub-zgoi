ed615f0a149f39c4dd63c8f4fd0292cdf77757d0
Rita-linlin <78244529+Rita-linlin@users.noreply.github.com>
2025-08-14 09:05:59 +0800

    Update 02_DecisionTree.py

--- 廖芮琳/week01/02_DecisionTree.py
+++ 廖芮琳/week01/02_DecisionTree.py
@@ -1,1 +1,25 @@
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.feature_extraction.text import HashingVectorizer
+
+import jieba
+import pandas as pd
+
+dataset  = pd.read_csv("dataset.csv", sep='\t', header=None)
+# print(dataset.head(5))
+
+input_sententce = dataset[0].apply(lambda x: " ".join(jieba.lcut(x)))
+
+vector = HashingVectorizer()
+vector.fit(input_sententce.values)
+input_feature = vector.transform(input_sententce.values)
+
+model = DecisionTreeClassifier()
+model.fit(input_feature, dataset[1].values)
+# print(model)
+
+user_input = input("请输入需要分类的文本:")
+test_sentence = " ".join(jieba.lcut(user_input))
+test_feature = vector.transform([test_sentence])
+print("待预测的文本:", user_input)
+print("决策树模型预测结果: ", model.predict(test_feature))
 
