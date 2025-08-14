40821a61d3e82714302be6837bc0634df52e0334
smoonlight <140109494@qq.com>
2025-08-14 22:13:17 +0800

    fix:修正代码逻辑

--- 徐吉祥/week01/text_classification.py
+++ 徐吉祥/week01/text_classification.py
@@ -7,28 +7,31 @@ from sklearn.svm import SVC
 
 #数据处理
 dataset = pd.read_csv("dataset.csv", sep="\t", header=None)
-input_sentence = dataset[0].apply(lambda x: " ".join(jieba.lcut(x)))
-vector = TfidfVectorizer()
-input_feature = vector.fit_transform(input_sentence.values)
-output = dataset[1].values
-test_query = "帮我播放一下郭德纲的小品"
-test_sentence = " ".join(jieba.lcut(test_query))
+
+x_sentence = dataset[0].apply(lambda x: " ".join(jieba.lcut(x)))
+y_target = dataset[1].values
+
+test_text = "帮我播放一下郭德纲的小品"
+test_x = " ".join(jieba.lcut(test_text))
 
 #贝叶斯模型
+tfidf_vector = TfidfVectorizer()
+x_feature = tfidf_vector.fit_transform(x_sentence)
 bs_model = MultinomialNB()
-bs_model.fit(input_feature, output)
+bs_model.fit(x_feature, y_target)
 
 #贝叶斯验证
-test_feature = vector.transform([test_sentence])
-print("待预测的文本", test_query)
+test_feature = tfidf_vector.transform([test_x])
+print("待预测的文本", test_text)
 print("贝叶斯模型预测结果: ", bs_model.predict(test_feature))
 
 #SVM模型
-vector = CountVectorizer()
-input_feature = vector.fit_transform(input_sentence.values)
-output = dataset[1].values
+count_vector = CountVectorizer()
+x_feature = count_vector.fit_transform(x_sentence)
+svm_model = SVC(kernel="linear", C=1.0, class_weight="balanced")
+svm_model.fit(x_feature, y_target)
 
 #SVM验证
-svm_model = SVC(kernel="linear", C=1.0, probability=True, class_weight="balanced")
-svm_model.fit(input_feature, output)
+test_feature = count_vector.transform([test_x])
+print("待预测的文本", test_text)
 print("svm模型预测结果: ", svm_model.predict(test_feature))
